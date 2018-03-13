6111f38925687b536827b0deb6b879606f10164c
mysm0722 <mysm0722@gmail.com>
2018-03-13 18:15:22 +0900

    Update app_server.js

--- app_server.js
+++ app_server.js
@@ -11,8 +11,8 @@ const cheerio = require('cheerio');
 var Tesseract = require('tesseract.js')
 
 var app = express();
-var client_id = 'EEMXt6n2ZpNPF3noBdSI';
-var client_secret = 'Tki7JNup5S';
+var client_id = '{YOUR_CLIENT_ID}';
+var client_secret = '{YOUR_SECRET_KEY}';
 
 var arvgStr = process.argv[2];
 var convStr;
